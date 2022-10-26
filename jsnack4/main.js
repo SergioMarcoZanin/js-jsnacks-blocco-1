fb252d9dbb6c7afc6ae8737cc882f2b2706c62c2
SergioMarcoZanin <zaninsergiomarco@gmail.com>
2022-10-26 12:17:52 +0200

    modifica script url

--- jsnack4/main.js
+++ jsnack4/main.js
@@ -3,7 +3,7 @@ const list=["Sergio","Marco","Bruno"];
 const name= prompt("Inserisci il tuo nome");
 let invitato = false;
 for (let i = 0; i < list.length; i++) {
-    if (name===list[i]){
+    if (namelucaluca===list[i]){
         alert("Accesso Riuscito");
         invitato=true;
         break
